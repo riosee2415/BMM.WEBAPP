7c6a32fc2beaafded0c954bb36144b7280ea9564
sts2003 <4leaf.sts@gmail.com>
2023-04-03 16:48:52 +0900

    review

---
+++ back/models/review.js
@@ -0,0 +1,56 @@
+const DataTypes = require("sequelize");
+const { Model } = DataTypes;
+
+module.exports = class Review extends Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        content: {
+          type: DataTypes.TEXT, // 내용
+          allowNull: false,
+        },
+        isImageReview: {
+          type: DataTypes.BOOLEAN, // 사진 리뷰 여부 (true이면 사진 여부 false라면 글 리뷰)
+          allowNull: false, // 사진 리뷰 라면 사진리뷰 포인트 1번만 받을 수 있도록 (즉, 사진 리뷰-> 글 리뷰로 변경할 수 없게 하도록 하기 위해 존재하는 컬럼)
+          defaultValue: false,
+        },
+        imagePath1: {
+          type: DataTypes.STRING(600), // 이미지1
+          allowNull: true,
+        },
+        imagePath2: {
+          type: DataTypes.STRING(600), // 이미지1
+          allowNull: true,
+        },
+        imagePath3: {
+          type: DataTypes.STRING(600), // 이미지1
+          allowNull: true,
+        },
+        imagePath4: {
+          type: DataTypes.STRING(600), // 이미지1
+          allowNull: true,
+        },
+        isDelete: {
+          type: DataTypes.BOOLEAN,
+          allowNull: false,
+          defaultValue: false,
+        },
+        deletedAt: {
+          type: DataTypes.DATE,
+          allowNull: true,
+        },
+      },
+      {
+        modelName: "Review",
+        tableName: "review",
+        charset: "utf8mb4",
+        collate: "utf8mb4_general_ci", // 한글 저장
+        sequelize,
+      }
+    );
+  }
+  static associate(db) {
+    db.Review.belongsTo(db.Product);
+    db.Review.belongsTo(db.User);
+  }
+};
---
+++ back/routers/reviewRouter.js
@@ -0,0 +1,409 @@
+const express = require("express");
+const models = require("../models");
+const isAdminCheck = require("../middlewares/isAdminCheck");
+const isLoggedIn = require("../middlewares/isLoggedIn");
+
+const router = express.Router();
+
+/**
+ * SUBJECT : 상품 별 리뷰 리스트
+ * PARAMETERS : page, ProductId
+ * ORDER BY : -
+ * STATEMENT : -
+ * DEVELOPMENT : 신태섭
+ * DEV DATE : 2023/04/03
+ */
+router.post("/product/list", async (req, res, next) => {
+  const { page, ProductId } = req.body;
+
+  const LIMIT = 5;
+
+  const _page = page ? page : 1;
+
+  const __page = _page - 1;
+  const OFFSET = __page * 5;
+
+  const lengthQuery = `
+  SELECT    ROW_NUMBER()    OVER(ORDER  BY A.createdAt)  AS num,
+            A.id,
+            A.content,
+            A.isImageReview,
+            A.imagePath1,
+            A.imagePath2,
+            A.imagePath3,
+            A.imagePath4,
+            A.createdAt,
+            A.updatedAt,
+            A.ProductId,
+            A.UserId,
+            B.userId                                     AS userLoginId,
+            B.username
+    FROM    review          A
+   INNER
+    JOIN    users           B
+      ON    A.UserId = B.id
+   WHERE    A.isDelete = 0
+     AND    A.ProductId = ${ProductId}
+   ORDER    BY num DESC
+  `;
+
+  const selectQuery = `
+  SELECT    ROW_NUMBER()    OVER(ORDER  BY A.createdAt)  AS num,
+            A.id,
+            A.content,
+            A.isImageReview,
+            A.imagePath1,
+            A.imagePath2,
+            A.imagePath3,
+            A.imagePath4,
+            A.createdAt,
+            A.updatedAt,
+            A.ProductId,
+            A.UserId,
+            B.userId                                     AS userLoginId,
+            B.username
+    FROM    review          A
+   INNER
+    JOIN    users           B
+      ON    A.UserId = B.id
+   WHERE    A.isDelete = 0
+     AND    A.ProductId = ${ProductId}
+   ORDER    BY num DESC
+   LIMIT    ${LIMIT}
+  OFFSET    ${OFFSET}
+  `;
+
+  try {
+    const lengths = await models.sequelize.query(lengthQuery);
+    const review = await models.sequelize.query(selectQuery);
+
+    const reviewLen = lengths[0].length;
+
+    const lastPage =
+      reviewLen % LIMIT > 0 ? reviewLen / LIMIT + 1 : reviewLen / LIMIT;
+
+    return res.status(200).json({
+      reviews: review[0],
+      lastPage: parseInt(lastPage),
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(401).send("리뷰 목록을 불러올 수 없습니다.");
+  }
+});
+
+/**
+ * SUBJECT : 자신이 작성한 리뷰 리스트
+ * PARAMETERS : page
+ * ORDER BY : -
+ * STATEMENT : -
+ * DEVELOPMENT : 신태섭
+ * DEV DATE : 2023/04/03
+ */
+router.post("/my/list", isLoggedIn, async (req, res, next) => {
+  const { page } = req.body;
+
+  const LIMIT = 10;
+
+  const _page = page ? page : 1;
+
+  const __page = _page - 1;
+  const OFFSET = __page * 10;
+
+  const lengthQuery = `
+  SELECT    ROW_NUMBER()    OVER(ORDER  BY A.createdAt)  AS num,
+            A.id,
+            A.content,
+            A.isImageReview,
+            A.imagePath1,
+            A.imagePath2,
+            A.imagePath3,
+            A.imagePath4,
+            A.createdAt,
+            A.updatedAt,
+            A.ProductId,
+            A.UserId,
+            B.userId                                     AS userLoginId,
+            B.username
+    FROM    review          A
+   INNER
+    JOIN    users           B
+      ON    A.UserId = B.id
+   WHERE    A.isDelete = 0
+     AND    A.UserId = ${req.user.id}
+   ORDER    BY num DESC
+  `;
+
+  const selectQuery = `
+  SELECT    ROW_NUMBER()    OVER(ORDER  BY A.createdAt)  AS num,
+            A.id,
+            A.content,
+            A.isImageReview,
+            A.imagePath1,
+            A.imagePath2,
+            A.imagePath3,
+            A.imagePath4,
+            A.createdAt,
+            A.updatedAt,
+            A.ProductId,
+            A.UserId,
+            B.userId                                     AS userLoginId,
+            B.username
+    FROM    review          A
+   INNER
+    JOIN    users           B
+      ON    A.UserId = B.id
+   WHERE    A.isDelete = 0
+     AND    A.UserId = ${req.user.id}
+   ORDER    BY num DESC
+   LIMIT    ${LIMIT}
+  OFFSET    ${OFFSET}
+  `;
+
+  try {
+    const lengths = await models.sequelize.query(lengthQuery);
+    const review = await models.sequelize.query(selectQuery);
+
+    const reviewLen = lengths[0].length;
+
+    const lastPage =
+      reviewLen % LIMIT > 0 ? reviewLen / LIMIT + 1 : reviewLen / LIMIT;
+
+    return res.status(200).json({
+      reviews: review[0],
+      lastPage: parseInt(lastPage),
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(401).send("리뷰 목록을 불러올 수 없습니다.");
+  }
+});
+
+/**
+ * SUBJECT : 관리자 리뷰 리스트
+ * PARAMETERS : ProductId, username
+ * ORDER BY : -
+ * STATEMENT : -
+ * DEVELOPMENT : 신태섭
+ * DEV DATE : 2023/04/03
+ */
+router.post("/admin/list", isAdminCheck, async (req, res, next) => {
+  const { ProductId, username } = req.body;
+
+  const _ProductId = parseInt(ProductId) || false;
+  const _username = username ? username : ``;
+
+  const selectQuery = `
+  SELECT    ROW_NUMBER()    OVER(ORDER  BY A.createdAt)  AS num,
+            A.id,
+            A.content,
+            A.isImageReview,
+            A.imagePath1,
+            A.imagePath2,
+            A.imagePath3,
+            A.imagePath4,
+            A.createdAt,
+            A.updatedAt,
+            A.ProductId,
+            A.UserId,
+            B.userId                                     AS userLoginId,
+            B.username
+    FROM    review          A
+   INNER
+    JOIN    users           B
+      ON    A.UserId = B.id
+   WHERE    A.isDelete = 0
+            ${_ProductId ? `AND A.ProductId = ${_ProductId}` : ``}
+            ${_username !== `` ? `AND B.username LIKE "%${_username}%"` : ``}
+   ORDER    BY num DESC
+  `;
+
+  try {
+    const review = await models.sequelize.query(selectQuery);
+
+    return res.status(200).json(review[0]);
+  } catch (error) {
+    console.error(error);
+    return res.status(401).send("리뷰 목록을 불러올 수 없습니다.");
+  }
+});
+
+/**
+ * SUBJECT : 리뷰 작성
+ * PARAMETERS : content,
+                isImageReview,
+                imagePath1,
+                imagePath2,
+                imagePath3,
+                imagePath4,
+                ProductId
+ * ORDER BY : -
+ * STATEMENT : -
+ * DEVELOPMENT : 신태섭
+ * DEV DATE : 2023/04/03
+ */
+router.post("/create", isLoggedIn, async (req, res, next) => {
+  const {
+    content,
+    isImageReview,
+    imagePath1,
+    imagePath2,
+    imagePath3,
+    imagePath4,
+    ProductId,
+  } = req.body;
+
+  const insertQuery = `
+  INSERT    INTO    review
+  (
+    content,
+    isImageReview,
+    imagePath1,
+    imagePath2,
+    imagePath3,
+    imagePath4,
+    ProductId,
+    UserId,
+    createdAt,
+    updatedAt
+  )
+  VALUES
+  (
+    "${content}",
+    ${isImageReview},
+    ${imagePath1 ? `"${imagePath1}"` : null},
+    ${imagePath2 ? `"${imagePath2}"` : null},
+    ${imagePath3 ? `"${imagePath3}"` : null},
+    ${imagePath4 ? `"${imagePath4}"` : null},
+    ${ProductId},
+    ${req.user.id},
+    NOW(),
+    NOW()
+  )
+  `;
+
+  try {
+    await models.sequelize.query(insertQuery);
+
+    if (isImageReview) {
+      // 사진 리뷰 일 때
+      return res.status(201).json({ result: true });
+    } else {
+      // 글 리뷰 일 때
+      return res.status(201).json({ result: true });
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(401).send("리뷰를 작성할 수 없습니다.");
+  }
+});
+
+/**
+ * SUBJECT : 리뷰 수정
+ * PARAMETERS : id, 
+                content,
+                imagePath1,
+                imagePath2,
+                imagePath3,
+                imagePath4
+ * ORDER BY : -
+ * STATEMENT : -
+ * DEVELOPMENT : 신태섭
+ * DEV DATE : 2023/04/03
+ */
+router.post("/update", isLoggedIn, async (req, res, next) => {
+  const { id, content, imagePath1, imagePath2, imagePath3, imagePath4 } =
+    req.body;
+
+  const selectQuery = `
+  SELECT    UserId,
+            isDelete
+    FROM    review
+   WHERE    id = ${id}
+  `;
+
+  const updateQuery = `
+  UPDATE    review
+     SET    "${content}",
+            ${imagePath1 ? `"${imagePath1}"` : null},
+            ${imagePath2 ? `"${imagePath2}"` : null},
+            ${imagePath3 ? `"${imagePath3}"` : null},
+            ${imagePath4 ? `"${imagePath4}"` : null},
+            updatedAt = NOW()
+   WHERE    id = ${id}
+  `;
+
+  try {
+    const findResult = await models.sequelize.query(selectQuery);
+
+    if (findResult[0].length === 0) {
+      return res.status(401).send("존재하지 않는 리뷰 정보입니다.");
+    }
+
+    if (findResult[0][0].isDelete) {
+      return res.status(401).send("이미 삭제된 리뷰입니다.");
+    }
+
+    if (findResult[0][0].UserId !== req.user.id) {
+      return res.status(401).send("자신이 작성한 리뷰만 수정할 수 있습니다.");
+    }
+
+    await models.sequelize.query(updateQuery);
+
+    return res.status(200).json({ result: true });
+  } catch (error) {
+    console.error(error);
+    return res.status(401).send("리뷰를 수정할 수 없습니다.");
+  }
+});
+
+/**
+ * SUBJECT : 리뷰 삭제
+ * PARAMETERS : id
+ * ORDER BY : -
+ * STATEMENT : -
+ * DEVELOPMENT : 신태섭
+ * DEV DATE : 2023/04/03
+ */
+router.post("/delete", isLoggedIn, async (req, res, next) => {
+  const { id } = req.body;
+
+  const selectQuery = `
+  SELECT    UserId,
+            isDelete
+    FROM    review
+   WHERE    id = ${id}
+  `;
+
+  const deleteQuery = `
+  UPDATE    review
+     SET    isDelete = 1,
+            deletedAt = NOW()
+   WHERE    id = ${id}
+  `;
+
+  try {
+    const findResult = await models.sequelize.query(selectQuery);
+
+    if (findResult[0].length === 0) {
+      return res.status(401).send("존재하지 않는 리뷰 정보입니다.");
+    }
+
+    if (findResult[0][0].isDelete) {
+      return res.status(401).send("이미 삭제된 리뷰입니다.");
+    }
+
+    if (findResult[0][0].UserId !== req.user.id) {
+      return res.status(401).send("자신이 작성한 리뷰만 삭제할 수 있습니다.");
+    }
+
+    await models.sequelize.query(deleteQuery);
+
+    return res.status(200).json({ result: true });
+  } catch (error) {
+    console.error(error);
+    return res.status(401).send("리뷰를 삭제할 수 없습니다.");
+  }
+});
+
+module.exports = router;
