adcc635e20c67b7e4d1dca61ab6ebcf475bd044b
4leafjhj <4leaf.jhj@gmail.com>
2023-04-04 10:50:35 +0900

    modify

--- front/pages/mypage/request.jsx
+++ front/pages/mypage/request.jsx
@@ -86,7 +86,7 @@ const Request = () => {
       </Head>
 
       <ClientLayout>
-        <WholeWrapper padding={width < 900 ? `40px 0 0` : `95px 0 0`}>
+        <WholeWrapper padding={width < 900 ? `40px 0 ` : `95px 0 `}>
           <RsWrapper>
             <MypageTop />
             <Wrapper
